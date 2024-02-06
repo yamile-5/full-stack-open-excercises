c2c968419285dab8a1e525b8877e1f97b1bb716b
Yamile Velazquez <yamilevelazquezgarcia@gmail.com>
2024-02-06 16:48:14 -0600

    refactor

--- part1/courses/src/App.jsx
+++ part1/courses/src/App.jsx
@@ -32,26 +32,28 @@ const Total = (props) =>{
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const  totalExercises = exercises1 + exercises2 + exercises3
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-        <Total total={totalExercises} />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+
     </div>
   )
 }
